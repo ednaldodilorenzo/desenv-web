52a7969d1a533bc52cf6b3c30cfcaf76a979085f
edsf <ednaldo.dilorenzo@gmail.com>
2026-03-12 09:14:43 -0300

    Alterações da atividade de criação de templates.

--- 2026/app-exemplo/app.py
+++ 2026/app-exemplo/app.py
@@ -1,30 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/usuarios/listar')
 def listar_usuarios():
-    return 'Listando usuários...'
+    return render_template('listar_usuarios.html')
 
-@app.route('/usuarios/cadastrar')
+@app.route('/usuarios/cadastrar', methods=['GET', 'POST'])
 def cadastrar_usuario():
-    return 'Cadastrando usuário...'
+    return render_template('cadastrar_usuario.html')
 
-@app.route('/usuarios/editar')
-def editar_usuario():
-    return 'Editando usuário...'
+@app.route('/usuarios/editar/<int:id>', methods=['GET', 'POST'])
+def editar_usuario(id):
+    return render_template('editar_usuario.html', id=id)
 
 @app.route('/administradores/listar')
 def listar_administradores():
-    return 'Listando administradores...'
+    return render_template('listar_administradores.html')
 
-@app.route('/administradores/cadastrar')
+@app.route('/administradores/cadastrar', methods=['GET', 'POST'])
 def cadastrar_administrador():
-    return 'Cadastrando administrador...'
+    return render_template('cadastrar_administrador.html')
 
-@app.route('/administradores/editar')
-def editar_administrador():
-    return 'Editando administrador...'
+@app.route('/administradores/editar/<int:id>', methods=['GET', 'POST'])
+def editar_administrador(id):
+    return render_template('editar_administrador.html', id=id)
 
 if __name__ == '__main__':
     app.run(debug=True)
